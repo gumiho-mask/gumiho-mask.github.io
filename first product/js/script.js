b05dba96a27c64303545ef35f5b8a83af477f867
gumiho <truyenhongoc95@hotmail.com>
2018-07-20 07:53:32 +0700

    Update vendors jquery mobile

--- first product/js/script.js
+++ first product/js/script.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function() {
+  $("#product__new--multi-item").swiperight(function() {
+     $(this).carousel('prev');
+   });
+  $("#product__new--multi-item").swipeleft(function() {
+     $(this).carousel('next');
+  });
+});
